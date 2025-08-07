195c2694819d6f7a7690bfb00823f3da911db703
mbkkt <mbkkt@users.noreply.github.com>
2025-08-07 15:53:18 +0000

    Fix AggregationPlan type

--- axiom/optimizer/QueryGraph.h
+++ axiom/optimizer/QueryGraph.h
@@ -790,7 +790,7 @@ using AggregationP = Aggregation*;
 /// Wraps an Aggregation RelationOp. This gives the aggregation a PlanObject id
 struct AggregationPlan : public PlanObject {
   AggregationPlan(AggregationP agg)
-      : PlanObject(PlanType::kAggregate), aggregation(agg) {}
+      : PlanObject(PlanType::kAggregation), aggregation(agg) {}
 
   AggregationP aggregation;
 };
